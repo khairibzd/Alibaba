47a36607a3c1618ea389c130ed6c17b2703206d4
khairibzd <khairibouzid95@gmail.com>
2024-08-25 21:13:28 +0100

    feat(component): add the chat user message components

---
+++ src/chatBot/ui/components/chat-user-message/index.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { User } from "lucide-react";
+import { ReactNode } from "react";
+
+export const ChatUserMessage = ({ children }: { children: ReactNode }) => {
+    return (
+      <>
+        <div className="flex items-start gap-3 justify-end">
+          <div className="bg-gray-100 p-3 rounded-lg max-w-[75%] text-primary-foreground rounded-tr-none text-sm text-gray-600">
+            {children}
+          </div>
+          <span className="rounded-full bg-gray-100 relative flex shrink-0 overflow-hidden  min-w-8 min-h-8 items-center justify-center text-gray-600">
+            <User />
+          </span>
+        </div>
+      </>
+    );
+  };
